5e1dddb55e6bdee43e04f2d13c400ab9b485eacf
13Legend31 <larrywu1996@hotmail.ca>
2018-07-20 14:03:11 -0400

    changes

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,43 +1,54 @@
 import React, {Component} from 'react';
-import Nav from './Nav/Nav';
-import Design from './Designs/Design'
-import './Designs/DesignPage.css'
+import Nav from './Nav/Nav.js';
+import About from './About/About.js';
+import './About/AboutPage.css'
 
-class DesignPage extends Component {
+class AboutPage extends Component {
     constructor(props) {
         super(props);
         this.onClick = this.onClick.bind(this);
+        this.addTimeout = this.addTimeout.bind(this);
         this.state = {
-            active:2
+            active:1
         }
         this.LoadPage = props.LoadPage;
+        this.timeouts = [];
+    }
+    addTimeout(timeout) {
+        this.timeouts.push(timeout);
     }
     componentDidMount() {
         const slider = document.getElementsByClassName('slider')[0];
+        console.log(slider);
         setTimeout(() => {
             slider.style.width = '0%';
             slider.style.left = '100%';
-        },200);
+        });
     }
     onClick(active) {
+        // Clear AboutMe Timeouts
+        for (let i = 0; i < this.timeouts.length; i++) {
+            clearTimeout(this.timeouts[i]);
+        }
+        // Transition to Design
         if (this.state.active !== active) {
             const state = this.state;
             state.active = active;
             const slider = document.getElementsByClassName('slider')[0];
             slider.style.width = '100%';
             slider.style.left = '0';
-            this.LoadPage(1);
+            this.LoadPage(2);
         }
     }
     render() {
         return (
             <section className = 'website'>
                 <Nav active = {this.state.active} onClick = {this.onClick}/>
-                <Design/>
+                <About addTimeout = {this.addTimeout}/>            
                 <div className = 'slider'></div>
             </section>
         );
     }
 }
 
-export default DesignPage;
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,37 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-import Nav from './Nav/Nav.js';
-import About from './About/About.js';
-import './index.css'
-import WebsiteDesigns from './DesignPage';
+import DesignPage from './DesignPage';
+import AboutPage from './AboutPage';
 
-class WebsiteAbout extends Component {
+class Website extends Component {
     constructor() {
         super();
-        this.onClick = this.onClick.bind(this);
-        this.addTimeout = this.addTimeout.bind(this);
+        this.LoadPage = this.LoadPage.bind(this);
         this.state = {
-            active:1
+            page:1
         }
-        this.timeouts = [];
     }
-    addTimeout(timeout) {
-        this.timeouts.push(timeout);
-    }
-    componentDidMount() {
-        const slider = document.getElementsByClassName('slider')[0];
-        setTimeout(() => {
-            slider.style.width = '0%';
-            slider.style.left = '100%';
-        },200);
-    }
-    onClick(active) {
-        // Clear AboutMe Timeouts
-        for (let i = 0; i < this.timeouts.length; i++) {
-            clearTimeout(this.timeouts[i]);
-        }
-        // Transition to Design
-        if (this.state.active !== active) {
-            const state = this.state;
-            state.active = active;
-            const slider = document.getElementsByClassName('slider')[0];
-            slider.style.width = '100%';
-            slider.style.left = '0';
-            // Design page
-            setTimeout(() => 
-                ReactDom.render(
-                    <WebsiteDesigns/>,
-                    document.getElementById('root')
-                )
-            ,500);
-            this.setState(state);
-        }
+    LoadPage(page) {
+        console.log(page);
+        setTimeout(() => 
+            this.setState({page:page})
+        ,500);
     }
     render() {
         return (
-            <section className = 'website'>
-                <Nav active = {this.state.active} onClick = {this.onClick}/>
-                <About addTimeout = {this.addTimeout}/>            
-                <div className = 'slider'></div>
-            </section>
+            <React.Fragment> 
+                {this.state.page === 1 &&   
+                    <AboutPage LoadPage = {this.LoadPage}/>      
+                }          
+                {this.state.page === 2 &&
+                    <DesignPage LoadPage = {this.LoadPage}/>
+                }
+            </React.Fragment>
         );
     }
 }
 
-export default WebsiteAbout;
-
 ReactDom.render( 
-    <WebsiteAbout/>,
+    <Website/>,
     document.getElementById('root')
 )
