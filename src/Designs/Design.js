03b55c2f5649ee56ea71f9bbb929b63645adc225
13Legend31 <larrywu1996@hotmail.ca>
2018-07-23 10:57:16 -0400

    improved scripts

--- src/Designs/Design.js
+++ src/Designs/Design.js
@@ -4,12 +4,17 @@ import RhythmTrack from './RhythmTrack.jpg'
 import Portfolio from './Portfolio.jpg'
 import ImageCollector from './ImageCollector.jpg'
 import ThisWebsite from './ThisWebsite.jpg'
+import MoreComingSoon from './MoreComingSoon.jpg'
 
 function Element(props) {
     const style = {
         backgroundImage:`url(${props.src})`
     }
-    return <div className = 'element' style = {style} onClick = {() => window.open(props.URL)}>
+    return <div className = 'element' style = {style} onClick = {() => {
+        if (props.URL) {
+            window.open(props.URL)
+        }
+    }}>
         <div className = 'elemName'>{props.name}</div>
     </div>
 }
@@ -20,6 +25,7 @@ function Elems() {
         <Element name = 'Portfolio' src = {Portfolio} URL = 'https://github.com/13Legend31/Previous-Website'/>
         <Element name = 'Image Collector' src = {ImageCollector} URL = 'https://github.com/13Legend31/Image-Collector'/>
         <Element name = 'This Website' src = {ThisWebsite} URL = 'https://github.com/13Legend31/Website'/>
+        <Element name = 'More Coming!' src = {MoreComingSoon}/>
     </section>
 }
 
